021b2348cebfe3f0012ba984ebda745c89c96e2d
Joshua Zeltman <joshua.zeltman@gmail.com>
2024-01-08 02:36:56 +0100

    tweak to main nav color mode positioning
    trying to optimize hero

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,4 +1,4 @@
-import { decorateButtons } from '../../scripts/aem.js';
+import { decorateButtons, createOptimizedPicture } from '../../scripts/aem.js';
 
 export default async function decorate(block) {
   decorateButtons(block);
@@ -9,10 +9,14 @@ export default async function decorate(block) {
   const picture = block.querySelector(':scope picture');
 
   if (picture) {
+    const src = picture.querySelector('img').getAttribute('src');
+    const alt = picture.querySelector('img').getAttribute('alt');
     picture.parentNode.replaceWith(picture);
     document.body.classList.add('has-hero');
-    picture.classList.add('hero-picture');
-    block.append(picture);
+    const newPicture = createOptimizedPicture(src, alt, true);
+    newPicture.classList.add('hero-picture');
+
+    block.append(newPicture);
   }
 
   const heroTextWrapper = document.createElement('div');
