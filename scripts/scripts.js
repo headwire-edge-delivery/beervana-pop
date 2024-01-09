94b150474311020bb125e558bb0721bdb011bebe
Joshua Zeltman <joshua.zeltman@gmail.com>
2024-01-09 14:18:22 +0100

    trying to switch lazy loaded images

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -185,6 +185,7 @@ async function loadEager(doc) {
   decorateTemplateAndTheme();
   const main = doc.querySelector('main');
   if (main) {
+    doc.querySelector('img')?.setAttribute('loading', 'eager');
     decorateMain(main);
     document.body.classList.add('appear');
     await waitForLCP(LCP_BLOCKS);
