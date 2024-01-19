cf93a0844cd1f1475f0aadf1bd504b5e61b5812c
Joshua Zeltman <joshua.zeltman@gmail.com>
2024-01-19 15:10:32 +0100

    reverting plugin code from non-functional example

--- tools/sidekick/link-validator.js
+++ tools/sidekick/link-validator.js
@@ -1,12 +1,7 @@
-/**
- * Called when a user tries to load the plugin
- * @param {HTMLElement} container The container to render the plugin in
- * @param {Object} data The data contained in the plugin sheet
- * @param {String} query If search is active, the current search query
- * @param {Object} context contains any properties set when the plugin was registered
- */
-export default async function decorate(container, data, query, context) {
-  // Render your plugin
-  /* eslint-disable no-console */
-  console.log('data', data, container, query, context);
+/* eslint-disable no-console */
+export default function linkValidator() {
+  console.log('link-validator.js');
+  console.log('window.hlx', window.hlx);
 }
+
+linkValidator();
