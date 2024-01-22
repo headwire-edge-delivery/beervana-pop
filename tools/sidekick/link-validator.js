caeab18fd0f3be6da4ef9bac4d8dee7354c07df2
Joshua Zeltman <joshua.zeltman@gmail.com>
2024-01-22 15:48:09 +0100

    added fallback for misconfigured url

--- tools/sidekick/link-validator.js
+++ tools/sidekick/link-validator.js
@@ -7,35 +7,42 @@ export default async function linkValidator() {
   const appScriptUrl = 'https://script.google.com/macros/s/AKfycbyf1E7HYTBa1fDn7SXIZPbJJlMxU4VxPqo4nTtkVEnllK7Fp0yrB_2RJYPIhWEvjZYK/exec';
   const urlParams = new URLSearchParams(window.location.search);
 
-  /* eslint-disable prefer-destructuring */
-  let documentId = urlParams.get('referrer');
-  documentId = documentId.split('https://docs.google.com/document/d/')[1];
-  documentId = documentId.split('/')[0];
-  const fetchUrl = `${appScriptUrl}?id=${documentId}`;
+  if (urlParams.size === 0) {
+    const listItem = document.createElement('li');
+    const listItemText = document.createTextNode('URL incorrectly configured');
+    listItem.appendChild(listItemText);
+    list.appendChild(listItem);
+  } else {
+    /* eslint-disable prefer-destructuring */
+    let documentId = urlParams.get('referrer');
+    documentId = documentId.split('https://docs.google.com/document/d/')[1];
+    documentId = documentId.split('/')[0];
+    const fetchUrl = `${appScriptUrl}?id=${documentId}`;
 
-  const response = await fetch(fetchUrl);
-  if (response.ok) {
-    const data = await response.json();
-    if (data) {
-      if (data.length === 0) {
-        const listItem = document.createElement('li');
-        const listItemText = document.createTextNode('No links found');
-        listItem.appendChild(listItemText);
-        list.appendChild(listItem);
-      } else {
-        const filteredData = data.filter(({ url }) => url.startsWith('https://docs.google.com/document'));
-        if (filteredData.length === 0) {
+    const response = await fetch(fetchUrl);
+    if (response.ok) {
+      const data = await response.json();
+      if (data) {
+        if (data.length === 0) {
           const listItem = document.createElement('li');
-          const listItemText = document.createTextNode('No invalid links found');
+          const listItemText = document.createTextNode('No links found');
           listItem.appendChild(listItemText);
           list.appendChild(listItem);
         } else {
-          filteredData.forEach(({ url }) => {
+          const filteredData = data.filter(({ url }) => url.startsWith('https://docs.google.com/document'));
+          if (filteredData.length === 0) {
             const listItem = document.createElement('li');
-            const listItemText = document.createTextNode(url);
+            const listItemText = document.createTextNode('No invalid links found');
             listItem.appendChild(listItemText);
             list.appendChild(listItem);
-          });
+          } else {
+            filteredData.forEach(({ url }) => {
+              const listItem = document.createElement('li');
+              const listItemText = document.createTextNode(url);
+              listItem.appendChild(listItemText);
+              list.appendChild(listItem);
+            });
+          }
         }
       }
     }
