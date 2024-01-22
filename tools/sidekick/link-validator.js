cd01cc4eee59a1c2021d8d2dd34938ae07555060
Joshua Zeltman <joshua.zeltman@gmail.com>
2024-01-22 10:50:32 +0100

    parsing the query params

--- tools/sidekick/link-validator.js
+++ tools/sidekick/link-validator.js
@@ -1,8 +1,7 @@
-/* eslint-disable no-console, comma-spacing */
-export default function linkValidator(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z) {
-  console.log('link-validator.js', a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z);
-  console.log('link-validator.js');
-  console.log('window.hlx', window.hlx);
+/* eslint-disable no-console */
+export default function linkValidator() {
+  const urlParams = new URLSearchParams(window.location.search);
+  console.log('urlParams', urlParams);
 }
 
 linkValidator();
