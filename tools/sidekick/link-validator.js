52593076d8b5dd839122d7e559758ed026c400b7
Joshua Zeltman <joshua.zeltman@gmail.com>
2024-01-22 11:00:53 +0100

    logging urlParams

--- tools/sidekick/link-validator.js
+++ tools/sidekick/link-validator.js
@@ -1,7 +1,10 @@
 /* eslint-disable no-console */
 export default function linkValidator() {
   const urlParams = new URLSearchParams(window.location.search);
-  console.log('urlParams', urlParams);
+  // loop over urlParams and console log their key/value pairs
+  urlParams.forEach((value, key) => {
+    console.log(`${key}: ${value}`);
+  });
 }
 
 linkValidator();
