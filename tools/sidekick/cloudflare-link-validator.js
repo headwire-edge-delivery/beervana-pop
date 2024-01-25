10050f772d4e2e94b339c10ed90a2b8dd0e3d0c5
Joshua Zeltman <joshua.zeltman@gmail.com>
2024-01-25 16:50:23 +0100

    added fetching cloudflare worker to validate current document

--- tools/sidekick/cloudflare-link-validator.js
+++ tools/sidekick/cloudflare-link-validator.js
@@ -10,9 +10,35 @@ export default async function linkValidator() {
     console.error('URL incorrectly configured');
   } else {
     /* eslint-disable prefer-destructuring */
-    // let documentId = urlParams.get('referrer');
-    // documentId = documentId.split('https://docs.google.com/document/d/')[1];
-    // documentId = documentId.split('/')[0];
+    const ref = urlParams.get('ref');
+    const repo = urlParams.get('repo');
+    const owner = urlParams.get('owner');
+    const referrer = urlParams.get('referrer');
+
+    if (ref && repo && owner && referrer) {
+      const previewUrl = `https://${ref}--${repo}--${owner}.hlx.page`;
+      const publishUrl = `https://${ref}--${repo}--${owner}.hlx.live`;
+      const documentId = referrer.split('https://docs.google.com/document/d/')[1]?.split('/')[0];
+
+      const workerUrl = new URL('https://eds-link-validator-worker.jz-759.workers.dev/');
+      workerUrl.searchParams.append('documentId', documentId);
+      workerUrl.searchParams.append('validate', true);
+      workerUrl.searchParams.append('previewUrl', previewUrl);
+      workerUrl.searchParams.append('publishUrl', publishUrl);
+
+      const response = await fetch(workerUrl.toString(), {
+        redirect: 'follow',
+        method: 'GET',
+        headers: {
+          'Content-Type': 'text/plain;charset=utf-8',
+        },
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log('data', data);
+      }
+    }
   }
 }
 
