382af7a1c4b57b835cf4fbfe4cb5eefc61bf2193
Joshua Zeltman <joshua.zeltman@gmail.com>
2024-01-30 08:30:09 +0100

    moved cloudflare-link-validator -> link-validator, and updated html include

--- tools/sidekick/link-validator.js
+++ tools/sidekick/link-validator.js
@@ -1,75 +1,172 @@
+const MIME_TYPES = {
+  DOC: 'application/vnd.google-apps.document',
+  SHEET: 'application/vnd.google-apps.spreadsheet',
+  FOLDER: 'application/vnd.google-apps.folder',
+};
+
+function createDocumentLinksHtml(links) {
+  return links.map((link) => `<li class="link">
+    <div class="link-title-wrapper">
+      <span class="links-toggle icon">
+        <input type="checkbox" class="icon" />
+        <span class="icon toggle">
+          <svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1 13L7 7L1 1" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+          </svg>
+        </span>
+      </span>
+      <strong class="link-title">${link.text}</strong>
+      <span class="icon link-validity check">
+        ${link.isValidUrl && !link.isEditorLink ? `
+        <svg class="valid" width="20" height="15" viewBox="0 0 20 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M18 2L7 13L2 8" stroke="#283618" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>` : `
+        <svg class="invalid" width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M13 1L1 13M1 1L13 13" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>`}
+      </span>
+    </div>
+    <div class="link-details">
+      <table>
+        <tbody>
+          <tr>
+            <td>Link URL</td>
+            <td>${link.url}</td>
+          </tr>
+          <tr>
+            <td>Link Text</td>
+            <td>${link.text}</td>
+          </tr>
+          <tr>
+            <td>Valid Site URL</td>
+            <td>${link.isValidSiteUrl}</td>
+          </tr>
+          <tr>
+            <td>External URL</td>
+            <td>${link.isExternalLink}</td>
+          </tr>
+          <tr>
+            <td>Editor URL</td>
+            <td>${link.isEditorLink}</td>
+          </tr>
+          <tr>
+            <td>Link Is Valid URL</td>
+            <td>${link.isValidUrl}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </li>`).join('');
+}
+
+export function createPagesHtml(files) {
+  return files.map((file) => `<li class="page">
+    <div class="page-title-wrapper">
+      <span class="links-toggle icon" data-has-links="${file.links?.length > 0}">
+        <input type="checkbox" class="icon" />
+        <span class="icon toggle">
+          <svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1 13L7 7L1 1" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+          </svg>
+        </span>
+      </span>
+      <strong class="page-title">${file.name}</strong>
+      ${file.links?.length === 0 ? `
+      <span class="icon no-links">
+        <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M3.93 3.93L18.07 18.07M21 11C21 16.5228 16.5228 21 11 21C5.47715 21 1 16.5228 1 11C1 5.47715 5.47715 1 11 1C16.5228 1 21 5.47715 21 11Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+      </span>` : `
+      <span class="icon page-validity">
+        <svg class="valid" width="20" height="15" viewBox="0 0 20 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M18 2L7 13L2 8" stroke="#283618" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+        <svg class="invalid" width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M13 1L1 13M1 1L13 13" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+      </span>`}
+    </div>
+    ${file.links && `<div class="page-links">
+      <ul class="links-container">${createDocumentLinksHtml(file.links)}</ul>
+    </div>`}
+  </li>`).join('');
+}
+
 /* eslint-disable no-console */
 export default async function linkValidator() {
-  const appContainer = document.getElementById('app');
-  const title = document.querySelector('#title');
-  const list = document.createElement('ul');
-  appContainer.appendChild(list);
-
-  const deployId = 'AKfycbzIdYez46Yqf21v7hoBCplIzUSiOHg7dq2BaRPo8Tq2BvOXQejgSFfDgdYIxuSvPSkr';
-  const appScriptUrl = `https://script.google.com/macros/s/${deployId}/exec`;
   const urlParams = new URLSearchParams(window.location.search);
+  const contentType = document.getElementById('content-type');
 
   if (urlParams.size === 0 || !urlParams.has('referrer')) {
-    title.innerText = 'URL incorrectly configured';
-    document.body.classList.add('error', 'loaded');
+    console.error('URL incorrectly configured');
   } else {
     /* eslint-disable prefer-destructuring */
-    let documentId = urlParams.get('referrer');
-    documentId = documentId.split('https://docs.google.com/document/d/')[1];
-    documentId = documentId.split('/')[0];
-    const fetchUrl = `${appScriptUrl}?id=${documentId}`;
+    const ref = urlParams.get('ref');
+    const repo = urlParams.get('repo');
+    const owner = urlParams.get('owner');
+    const referrer = urlParams.get('referrer');
 
-    try {
-      const response = await fetch(fetchUrl, {
-        redirect: 'follow',
-        method: 'GET',
-        headers: {
-          'Content-Type': 'text/plain;charset=utf-8',
-        },
-      });
-      if (response.ok) {
-        const data = await response.json();
-        if (data) {
-          if (data.length === 0) {
-            title.innerText = 'No links found';
+    if (ref && repo && owner && referrer) {
+      const previewUrl = `https://${ref}--${repo}--${owner}.hlx.page`;
+      const publishUrl = `https://${ref}--${repo}--${owner}.hlx.live`;
+      const referrerUrl = new URL(referrer);
+
+      const workerUrl = new URL('https://eds-link-validator-worker.jz-759.workers.dev/');
+      workerUrl.searchParams.append('validate', true);
+      workerUrl.searchParams.append('previewUrl', previewUrl);
+      workerUrl.searchParams.append('publishUrl', publishUrl);
+
+      if (referrerUrl.pathname.includes('folders')) {
+        contentType.innerHTML = 'Folder';
+        const folderId = referrerUrl.pathname.split('/folders/')[1]?.split('/')[0];
+        workerUrl.searchParams.append('folderId', folderId);
+
+        const response = await fetch(workerUrl.toString(), {
+          redirect: 'follow',
+          method: 'GET',
+          headers: {
+            'Content-Type': 'text/plain;charset=utf-8',
+          },
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          const titleEl = document.querySelector('#title');
+          titleEl.innerHTML = 'Link Validator for current folder';
+          const appContainer = document.querySelector('#app');
+          const documents = data.filter((file) => file.mimeType === MIME_TYPES.DOC);
+          appContainer.innerHTML = `<ul class="links-container">${createPagesHtml(documents)}</ul>`;
+          document.body.classList.add('loaded');
+        } else {
+          console.error('response not ok', response);
+        }
+      } else {
+        contentType.innerHTML = 'Document';
+        const documentId = referrerUrl.pathname.split('/document/d/')[1]?.split('/')[0];
+        workerUrl.searchParams.append('documentId', documentId);
+
+        const response = await fetch(workerUrl.toString(), {
+          redirect: 'follow',
+          method: 'GET',
+          headers: {
+            'Content-Type': 'text/plain;charset=utf-8',
+          },
+        });
+
+        if (response.ok) {
+          const { title, links } = await response.json();
+          const titleEl = document.querySelector('#title');
+          titleEl.innerHTML = `Link Validator for ${title}`;
+          const appContainer = document.querySelector('#app');
+          if (links.length === 0) {
+            appContainer.innerHTML = '<h2>No Links Found</h2>';
           } else {
-            const filteredData = data.filter(({ url }) => url.startsWith('https://docs.google.com/document'));
-            if (filteredData.length === 0) {
-              title.innerText = 'No invalid links found';
-            } else {
-              title.innerText = 'Invalid links';
-              filteredData.forEach(({
-                endOffsetInclusive,
-                startOffset,
-                text,
-                url,
-              }) => {
-                const linkText = text.substring(startOffset, endOffsetInclusive + 1);
-                const listItem = document.createElement('li');
-                const listItemLink = document.createElement('a');
-                listItemLink.setAttribute('href', url);
-                const listItemText = document.createTextNode(linkText);
-                const listItemSpan = document.createElement('span');
-                const listItemUrl = document.createTextNode(url);
-                listItemLink.appendChild(listItemText);
-                listItem.appendChild(listItemLink);
-                listItemSpan.appendChild(listItemUrl);
-                listItem.appendChild(listItemSpan);
-                list.appendChild(listItem);
-              });
-            }
+            appContainer.innerHTML = `<ul class="links-container">${createDocumentLinksHtml(links)}</ul>`;
           }
+          document.body.classList.add('loaded');
         }
       }
-    } catch (error) {
-      console.log(error);
-      const listItem = document.createElement('li');
-      const listItemText = document.createTextNode('An error occurred while trying to fetch the data');
-      listItem.appendChild(listItemText);
-      list.appendChild(listItem);
     }
-
-    document.body.classList.add('loaded');
   }
 }
 
