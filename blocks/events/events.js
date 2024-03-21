ebb9476e9c5ed15c1dce4a0db74944bb04bede65
Joshua Zeltman <joshua.zeltman@gmail.com>
2024-03-21 10:49:06 +0100

    moved cards css to its own file again
    moved events css to its own file
    
    updated templates file to not have helper mapping functions
    
    TODO: test performance with css in their own files

---
+++ blocks/events/events.js
@@ -0,0 +1,4 @@
+export default async function decorate() {
+  // this component is not currently used here
+  // Look to the dynamic content block
+}
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,11 +1,8 @@
 import { createOptimizedPicture } from './aem.js';
 
-export function templateCard({
-  description,
-  image,
-  path,
-  title,
-}, index, placeholders, config) {
+/* eslint-disable object-curly-newline */
+export function templateCards({ item, index, placeholders, config }) {
+  const { description, image, path, title } = item;
   const breakpoints = [{ media: '(min-width: 600px)', width: '500' }, { width: '300' }];
   const { emptyLinkTitlePrefix, cardButtonText } = placeholders;
   return `<div class="card" data-index="${index}">
@@ -32,18 +29,31 @@ export function templateCard({
   </div>`;
 }
 
-export function templateCards({ data, config, placeholders }) {
-  const items = data.map(({
-    description,
-    image,
-    path,
-    title,
-  }, index) => templateCard({
-    description,
-    image,
-    path,
-    title,
-  }, index, placeholders, config))
-    .join('');
-  return `<div class="cards ${config.variant}">${items}</div>`;
+export function templateEvents({ item, index, placeholders }) {
+  const { description, image, path, title, startDate } = item;
+  const breakpoints = [{ media: '(min-width: 600px)', width: '350' }, { width: '350' }];
+  const { eventsButtonText } = placeholders;
+  const date = new Date(startDate);
+  return `<div class="event event-${index}">
+    <div class="events-image image-content" style="--background-image: url(${image})">
+      <a href="${path}" title="${title}">
+        <span class="border-group">
+          ${createOptimizedPicture(image, '', index < 3, breakpoints).outerHTML}
+          <span class="date">
+            <span class="weekday">${date.toLocaleDateString('en-US', { weekday: 'long' })}</span>
+            <span class="day">${date.toLocaleDateString('en-US', { day: 'numeric' })}</span>
+          </span>
+        </span>
+      </a>
+    </div>
+    <div class="events-body">
+      <h3>
+        <a href="${path}" title="${title}">${title}</a>
+      </h3>
+      <p>${description}</p>
+      <p class="button-container">
+        <a href="${path}" class="button">${eventsButtonText}</a>
+      </p>
+    </div>
+  </div>`;
 }
