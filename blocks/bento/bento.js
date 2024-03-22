d7a0b2d8386a14181bbfe33f7519c03f79aa0988
Joshua Zeltman <joshua.zeltman@gmail.com>
2024-03-22 10:29:49 +0100

    functional bento grid from table, with correct layout and supporting layout styles

--- blocks/bento/bento.js
+++ blocks/bento/bento.js
@@ -1,72 +1,71 @@
+/* eslint-disable no-return-assign */
+function getBreakpointConfig(table) {
+  const config = {};
+  const tableMatrix = [];
+  table.querySelectorAll('tr')?.forEach((_, rowIndex) => tableMatrix[rowIndex] = []);
+  table.querySelectorAll('tr')?.forEach((row, rowIndex) => {
+    row.querySelectorAll('td')?.forEach((cell, cellIndex) => {
+      const key = cell.textContent.trim().toLowerCase();
+      config[key] = {
+        columnStart: 0,
+        columnSpan: cell.colSpan || 1,
+        rowStart: rowIndex + 1,
+        rowSpan: cell.rowSpan || 1,
+      };
+
+      /* eslint-disable no-plusplus */
+      for (let i = rowIndex; i < rowIndex + config[key].rowSpan; i++) {
+        for (let j = cellIndex; j < cellIndex + config[key].columnSpan; j++) {
+          if (!tableMatrix[i][j]) {
+            tableMatrix[i][j] = key;
+            if (config[key].columnStart === 0) config[key].columnStart = j + 1;
+          } else {
+            if (config[key].columnStart === 0) config[key].columnStart = tableMatrix[i].length + 1;
+            tableMatrix[i].push(key);
+          }
+        }
+      }
+    });
+  });
+  config.tableMatrix = tableMatrix;
+  config.columns = tableMatrix[0].length;
+  config.rows = tableMatrix.length;
+
+  return config;
+}
+
+function getConfig(block, config) {
+  block.querySelectorAll('table')?.forEach((table) => {
+    let breakpoint = table.parentElement.querySelector('p')?.textContent.trim().toLowerCase() || 'desktop';
+    breakpoint = breakpoint.indexOf(',') === -1 ? [breakpoint] : breakpoint.split(',');
+    breakpoint.forEach((bp) => config[bp.trim()] = getBreakpointConfig(table));
+    config.table = table;
+    table?.parentElement?.parentElement?.remove();
+  });
+
+  return config;
+}
+
 export default async function decorate(block) {
-  block.querySelector('img')?.setAttribute('loading', 'eager');
-
-  const INTERVAL_TIME = 4000;
-  function setActiveStory(target) {
-    const stories = document.querySelector('.stories-wrapper');
-    const storiesNavigation = document.querySelector('.stories-navigation');
-    const currentStory = stories.querySelector('.active');
-    const currentStoryNav = storiesNavigation.querySelector('.active');
-    const nextStory = target ? stories.querySelector(`[data-story="${target.dataset.story}"]`) : currentStory.nextElementSibling || stories.querySelector('.story-wrapper:first-child');
-    const nextStoryNav = target || currentStoryNav.nextElementSibling || storiesNavigation.querySelector('.story-nav:first-child');
-    currentStory.classList.remove('active');
-    currentStoryNav.classList.remove('active');
-    nextStory.classList.add('active');
-    nextStoryNav.classList.add('active');
-  }
-
-  let storyInterval = setInterval(() => setActiveStory(), INTERVAL_TIME);
-
-  const storiesNavigation = document.createElement('div');
-
-  const elements = block.querySelectorAll(':scope > div');
-  elements.forEach((element) => {
+  const config = getConfig(block, {});
+  // set the config on the block
+  Object.keys(config).forEach((key) => {
+    const { columns, rows } = config[key];
+    block.style.setProperty(`--grid-columns-${key}`, columns);
+    block.style.setProperty(`--grid-rows-${key}`, rows);
+  });
+  // set the config on the items
+  block.querySelectorAll(':scope > div').forEach((element) => {
     element.classList.add('bento-item');
-    const contents = element.querySelectorAll(':scope > div');
-    const classes = contents[1];
-    if (classes) {
-      element.classList.add(...classes.innerText.toLowerCase().split(' '));
-      classes.remove();
-    }
-
-    if (element.querySelector('picture')) {
-      element.classList.add('has-image', 'no-padding');
-    }
-
-    element.addEventListener('mousemove', ({ target, pageX, pageY }) => {
-      const bentoItem = target?.closest('.bento-item');
-      const x = pageX - bentoItem.offsetLeft;
-      const y = pageY - bentoItem.offsetTop;
-      bentoItem.style.setProperty('--left', `${x}px`);
-      bentoItem.style.setProperty('--top', `${y}px`);
+    const key = element.children[0]?.textContent.trim().toLowerCase();
+    Object.keys(config).forEach((bp) => {
+      if (config[bp][key]) {
+        element.style.setProperty(`--grid-column-start-${bp}`, config[bp][key].columnStart);
+        element.style.setProperty(`--grid-column-end-${bp}`, config[bp][key].columnSpan);
+        element.style.setProperty(`--grid-row-start-${bp}`, config[bp][key].rowStart);
+        element.style.setProperty(`--grid-row-end-${bp}`, config[bp][key].rowSpan);
+      }
     });
+    element.children[0].remove();
   });
-
-  block.querySelectorAll('.stories')?.forEach((storyContainer) => {
-    storiesNavigation.classList.add('stories-navigation');
-
-    const storiesWrapper = storyContainer.querySelector(':scope > div');
-    storiesWrapper.classList.add('stories-wrapper');
-
-    storiesWrapper.querySelectorAll('p')?.forEach((storyWrapper, i) => {
-      storyWrapper.classList.add('story-wrapper', i === 0 && 'active');
-      storyWrapper.dataset.story = i;
-      storyWrapper.querySelector('img')?.removeAttribute('loading');
-
-      const storyNav = document.createElement('button');
-      storyNav.dataset.story = i;
-      storyNav.classList.add('story-nav', i, i === 0 && 'active');
-      storyNav.setAttribute('title', `Navigate to story ${i + 1}`);
-      storyNav.addEventListener('click', ({ target }) => {
-        clearInterval(storyInterval);
-        setActiveStory(target);
-        storyInterval = setInterval(() => setActiveStory(), INTERVAL_TIME);
-      });
-      storiesNavigation.appendChild(storyNav);
-    });
-
-    storyContainer.appendChild(storiesNavigation);
-  });
-
-  block.querySelectorAll('hr')?.forEach((hr) => hr.remove());
 }
